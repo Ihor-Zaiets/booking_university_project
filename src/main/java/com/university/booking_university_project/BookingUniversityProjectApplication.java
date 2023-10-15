16d59220a1ab808539852106a555a0fb5db23b5d
Ihor Zaiets <dekstroer8202@gmail.com>
2023-10-15 17:55:42 +0200

    project init

---
+++ src/main/java/com/university/booking_university_project/BookingUniversityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.university.booking_university_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingUniversityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingUniversityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/booking_university_project/BookingUniversityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.booking_university_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingUniversityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
