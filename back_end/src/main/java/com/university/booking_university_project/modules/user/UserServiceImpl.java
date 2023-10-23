3097ff29d9734d519f9762268b8382682ab803af
Ihor Zaiets <dekstroer8202@gmail.com>
2023-10-23 23:15:09 +0200

    userService save() test and implementation

--- back_end/src/main/java/com/university/booking_university_project/modules/user/UserServiceImpl.java
+++ back_end/src/main/java/com/university/booking_university_project/modules/user/UserServiceImpl.java
@@ -1,8 +1,21 @@
 package com.university.booking_university_project.modules.user;
 
+import com.university.booking_university_project.jpa.entity.User;
+import com.university.booking_university_project.modules.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
--- back_end/src/test/java/com/university/booking_university_project/modules/UserServiceImplTest.java
+++ back_end/src/test/java/com/university/booking_university_project/modules/UserServiceImplTest.java
@@ -1,16 +1,38 @@
 package com.university.booking_university_project.modules;
 
+import com.university.booking_university_project.jpa.entity.User;
 import com.university.booking_university_project.modules.user.UserServiceImpl;
+import com.university.booking_university_project.modules.user.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Transactional
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 public class UserServiceImplTest {
 
     private final UserServiceImpl userServiceImpl;
+    
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserServiceImplTest(UserServiceImpl userServiceImpl) {
+    public UserServiceImplTest(UserServiceImpl userServiceImpl, UserRepository userRepository) {
         this.userServiceImpl = userServiceImpl;
+        this.userRepository = userRepository;
+    }
+
+    @Test
+    public void save(){
+        User user = new User();
+        user = userServiceImpl.save(user);
+        Optional<User> userOptional = userRepository.findById(user.getId());
+        Assertions.assertTrue(userOptional.isPresent());
     }
 }
