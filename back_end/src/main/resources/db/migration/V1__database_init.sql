b833973b87df8900f444c393282b944f875e680a
Ihor Zaiets <dekstroer8202@gmail.com>
2023-10-30 21:29:28 +0100

    reservation_status bug fix

--- back_end/src/main/resources/db/migration/V1__database_init.sql
+++ back_end/src/main/resources/db/migration/V1__database_init.sql
@@ -5,6 +5,8 @@ CREATE TYPE public.reservation_status AS ENUM (
     'FINISHED',
     'UNFINISHED');
 
+CREATE CAST (character varying AS public.reservation_status) WITH INOUT AS IMPLICIT;
+
 CREATE TABLE public.apartment (
                                   id serial primary key ,
                                   number_of_rooms integer NOT NULL,
@@ -32,6 +34,6 @@ CREATE TABLE public.reservation (
                                     start_date timestamp without time zone NOT NULL,
                                     end_date timestamp without time zone NOT NULL,
                                     number_of_people integer NOT NULL,
-                                    reservation_status text NOT NULL,
+                                    reservation_status public.reservation_status NOT NULL,
                                     price numeric(10,2) NOT NULL
 );
