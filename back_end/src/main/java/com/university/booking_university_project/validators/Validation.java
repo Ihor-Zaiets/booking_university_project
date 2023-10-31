ef7752d9a35fefa45e8a275ec87e9342399d855d
Ihor Zaiets <dekstroer8202@gmail.com>
2023-10-31 14:20:59 +0100

    user field validations (TDD)

--- back_end/src/main/java/com/university/booking_university_project/validators/Validation.java
+++ back_end/src/main/java/com/university/booking_university_project/validators/Validation.java
@@ -1,3 +1,35 @@
 package com.university.booking_university_project.validators;
 
-public class Validation {}
+import com.university.booking_university_project.exception.ValidationException;
+
+public class Validation {
+
+  public final static String ONLY_NUMBERS_REGEX = "\\d+";
+  public final static String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+  public final static String PHONE_REGEX = "^(\\+\\d{1,3}\\s*)?\\d{9}$";
+
+  public static void validateTrimSpaces(String string) {
+    if (!string.trim().equals(string))
+      throw new ValidationException("String contains white spaces at beginning or end");
+  }
+
+  public static void validateUserFirstName(String firstName) {
+    if (firstName == null || firstName.isBlank() || firstName.matches(ONLY_NUMBERS_REGEX))
+      throw new ValidationException("User firstname is null or empty");
+  }
+
+  public static void validateUserSurname(String userSurname) {
+    if (userSurname == null || userSurname.isBlank() || userSurname.matches(ONLY_NUMBERS_REGEX))
+      throw new ValidationException("User surname is null or empty");
+  }
+
+  public static void validateEmail(String userEmail) {
+    if (userEmail != null && !userEmail.matches(EMAIL_REGEX))
+      throw new ValidationException("Email does not match pattern");
+  }
+
+  public static void validatePhone(String userPhone) {
+    if (!userPhone.replaceAll(" ", "").matches(PHONE_REGEX))
+      throw new ValidationException("Phone number does not match pattern");
+  }
+}
--- back_end/src/test/java/com/university/booking_university_project/validators/ValidationTest.java
+++ back_end/src/test/java/com/university/booking_university_project/validators/ValidationTest.java
@@ -1,10 +1,125 @@
 package com.university.booking_university_project.validators;
 
 import com.university.booking_university_project.exception.ValidationException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ValidationTest {
 
+  @Test
+  public void validateTrimSpacesThrowIfContains() {
+    String beginning = " string";
+    String end = "string ";
+    String both = " string ";
+    String multiple = "   string   ";
+
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateTrimSpaces(beginning));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateTrimSpaces(end));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateTrimSpaces(both));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateTrimSpaces(multiple));
+  }
+
+  @Test
+  public void validateTrimSpacesNotThrowIfDoesNotContains() {
+    String string = "string";
+
+    Assertions.assertDoesNotThrow(() -> Validation.validateTrimSpaces(string));
+  }
+
+  @Test
+  public void validateUserFirstNameThrowIfUserFirstnameIsNullOrBlank() {
+    String userFirstName = "";
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateUserFirstName(userFirstName));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateUserFirstName(null));
+  }
+
+  @Test
+  public void validateUserFirstNameNotThrowIfUserFirstnameIsNotNullOrBlank() {
+    String userFirstName = "name";
+    Assertions.assertDoesNotThrow(() -> Validation.validateUserFirstName(userFirstName));
+  }
+
+  @Test
+  public void validateUserFirstnameThrowIfUserFirstNameContainsOnlyNumbers() {
+    String userFirstName = "34598632";
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateUserFirstName(userFirstName));
+  }
+
+  @Test
+  public void validateUserSurnameThrowIfUserSurnameNullOrBlank() {
+    String userSurname = "";
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateUserSurname(userSurname));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateUserSurname(null));
+  }
+
+  @Test
+  public void validateUserSurnameNotThrowIfUserSurnameIsNotNullOrBlank() {
+    String userSurname = "surname";
+    Assertions.assertDoesNotThrow(() -> Validation.validateUserSurname(userSurname));
+  }
+
+  @Test
+  public void validateUserSurnameThrowIfUserSurnameContainsOnlyNumbers() {
+    String userSurname = "134667536432";
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateUserSurname(userSurname));
+  }
+
+  @Test
+  public void validateUserEmailDoNothingIfEmailIsNull() {
+    Assertions.assertDoesNotThrow(() -> Validation.validateEmail(null));
+  }
+
+  @Test
+  public void validateUserEmailThrowIfEmailDoesNotMatchPattern() {
+    String emptyUserEmail = "";
+    String justString = "string";
+    String emailWithoutDomain= "string@string";
+    String emailWithShortDomain= "string@str.ing.c";
+    String emailWithoutAT= "string.string";
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateEmail(emptyUserEmail));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateEmail(justString));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateEmail(emailWithoutDomain));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateEmail(emailWithShortDomain));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validateEmail(emailWithoutAT));
+  }
+
+  @Test
+  public void validateUserEmailNotThrowIfUserEmailMatchesPattern() {
+    String exampleEmail = "example@email.com";
+    String differentDomain = "domain@string.differentdomain";
+    String withNumbers = "emailNr9000@gmail.yonk";
+    String multipleDomains = "emailNr9000@gmail.gmail2.yonk";
+
+    Assertions.assertDoesNotThrow(() -> Validation.validateEmail(exampleEmail));
+    Assertions.assertDoesNotThrow(() -> Validation.validateEmail(differentDomain));
+    Assertions.assertDoesNotThrow(() -> Validation.validateEmail(withNumbers));
+    Assertions.assertDoesNotThrow(() -> Validation.validateEmail(multipleDomains));
+  }
+
+  @Test
+  public void validateUserPhoneNotThrowIfUserPhoneMatchesPattern() {
+    String userPhone = "123456789";
+    String userPhoneWithCode = "+48123456789";
+    String userPhoneWithSpaces = "123 456 78 9";
+    Assertions.assertDoesNotThrow(() -> Validation.validatePhone(userPhone));
+    Assertions.assertDoesNotThrow(() -> Validation.validatePhone(userPhoneWithCode));
+    Assertions.assertDoesNotThrow(() -> Validation.validatePhone(userPhoneWithSpaces));
+  }
+
+  @Test
+  public void validateUserPhoneThrowIfUserNumberPhoneDoesNotMatchPattern() {
+    String empty = "";
+    String string = "string";
+    String stringWithNumbers = "+str1ngWith5624";
+    String tooLong = "12345678900987654321";
+    String tooShort = "12345";
+
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validatePhone(empty));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validatePhone(string));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validatePhone(stringWithNumbers));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validatePhone(tooLong));
+    Assertions.assertThrows(ValidationException.class, () -> Validation.validatePhone(tooShort));
+  }
 }
