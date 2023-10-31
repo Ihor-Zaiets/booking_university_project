2e938daaaf38cbbe78d0831df725b2a4d76b9658
Ihor Zaiets <dekstroer8202@gmail.com>
2023-10-31 12:46:20 +0100

    test refactor
    delete dependencies on User and Apartment Repository.
    When user CRUD is not working, either does reservation CRUD. It`s wrong, so I rewrite some code

--- back_end/src/test/java/com/university/booking_university_project/modules/reservation/ReservationServiceImplTest.java
+++ back_end/src/test/java/com/university/booking_university_project/modules/reservation/ReservationServiceImplTest.java
@@ -4,18 +4,15 @@ import com.university.booking_university_project.jpa.entity.Apartment;
 import com.university.booking_university_project.jpa.entity.Reservation;
 import com.university.booking_university_project.jpa.entity.User;
 import com.university.booking_university_project.jpa.enums.ReservationStatus;
-import com.university.booking_university_project.modules.apartment.ApartmentServiceImplTest;
-import com.university.booking_university_project.modules.apartment.repository.ApartmentRepository;
 import com.university.booking_university_project.modules.baseService.BaseServiceTest;
 import com.university.booking_university_project.modules.reservation.repository.ReservationRepository;
-import com.university.booking_university_project.modules.user.UserServiceImplTest;
-import com.university.booking_university_project.modules.user.repository.UserRepository;
-import com.university.booking_university_project.testUtils.TestUtils;
-import org.checkerframework.checker.units.qual.A;
+import jakarta.persistence.EntityManager;
+import java.sql.Timestamp;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.sql.Timestamp;
 
 @SpringBootTest
 public class ReservationServiceImplTest
@@ -23,11 +20,7 @@ public class ReservationServiceImplTest
 
   private final ReservationRepository reservationRepository;
 
-  private final ApartmentRepository apartmentRepository;
-
-  private final UserRepository userRepository;
-
-  private final TestUtils testUtils;
+  private final EntityManager entityManager;
 
   private static final Timestamp TEST_START_DATE = Timestamp.valueOf("2023-01-01 12:00:00");
   private static final Timestamp TEST_END_DATE = Timestamp.valueOf("2023-01-03 12:00:00");
@@ -38,13 +31,9 @@ public class ReservationServiceImplTest
   @Autowired
   public ReservationServiceImplTest(
       ReservationRepository reservationRepository,
-      ApartmentRepository apartmentRepository,
-      UserRepository userRepository,
-      TestUtils testUtils) {
+      EntityManager entityManager) {
     this.reservationRepository = reservationRepository;
-    this.apartmentRepository = apartmentRepository;
-    this.userRepository = userRepository;
-    this.testUtils = testUtils;
+    this.entityManager = entityManager;
   }
 
   @Override
@@ -54,9 +43,14 @@ public class ReservationServiceImplTest
 
   @Override
   public Reservation createEntity() {
+    Apartment apartment = new Apartment();
+    User user = new User();
+    apartment.setId(0);
+    user.setId(0);
+
     Reservation reservation = new Reservation();
-    reservation.setApartment(createApartment());
-    reservation.setUser(createUser());
+    reservation.setApartment(apartment);
+    reservation.setUser(user);
     reservation.setStart_date(TEST_START_DATE);
     reservation.setEnd_date(TEST_END_DATE);
     reservation.setNumberOfPeople(TEST_NUMBER_OF_PEOPLE);
@@ -65,25 +59,11 @@ public class ReservationServiceImplTest
     return reservation;
   }
 
-  private Apartment createApartment() {
-    return apartmentRepository.save(
-        testUtils.createApartmentEntity(
-            ApartmentServiceImplTest.TEST_NUMBER_OF_ROOMS,
-            ApartmentServiceImplTest.TEST_SQUARE,
-            ApartmentServiceImplTest.TEST_RENT_PRICE,
-            ApartmentServiceImplTest.TEST_FLOOR,
-            ApartmentServiceImplTest.TEST_DESCRIPTION,
-            ApartmentServiceImplTest.TEST_NUMBER_OF_DOUBLE_BEDS,
-            ApartmentServiceImplTest.TEST_NUMBER_OF_SINGLE_BEDS));
-  }
-
-  private User createUser() {
-    return userRepository.save(
-        testUtils.createUserEntity(
-            UserServiceImplTest.TEST_USER_NAME,
-            UserServiceImplTest.TEST_USER_SURNAME,
-            UserServiceImplTest.TEST_USER_EMAIL,
-            UserServiceImplTest.TEST_USER_PHONE,
-            UserServiceImplTest.TEST_USER_ADDRESS));
+  @BeforeEach
+  public void setUp() {
+    Logger logger = LoggerFactory.getLogger(ReservationServiceImplTest.class);
+    logger.info("before each< find me");
+    String disableConstraintsQuery = "ALTER TABLE public.reservation DISABLE TRIGGER ALL;";
+    entityManager.createNativeQuery(disableConstraintsQuery).executeUpdate();
   }
 }
