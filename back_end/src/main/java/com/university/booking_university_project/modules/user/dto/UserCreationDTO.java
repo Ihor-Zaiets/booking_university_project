9c17e2862579351cbd4abcea84f92ac0e19c1a71
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-02 19:58:50 +0100

    UserCreationDTO create

---
+++ back_end/src/main/java/com/university/booking_university_project/modules/user/dto/UserCreationDTO.java
@@ -0,0 +1,19 @@
+package com.university.booking_university_project.modules.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+
+@Getter
+@Setter
+public class UserCreationDTO {
+  @NonNull private String firstname;
+
+  private String surname;
+
+  private String email;
+
+  @NonNull private String phone;
+
+  private String address;
+}
