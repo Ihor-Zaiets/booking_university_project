1eeebaebfa0ff0cf286ec1a46446804ad429cf94
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-07 20:39:08 +0100

    unfinished mockito

--- back_end/src/test/java/com/university/booking_university_project/modules/apartment/ApartmentServiceImplTest.java
+++ back_end/src/test/java/com/university/booking_university_project/modules/apartment/ApartmentServiceImplTest.java
@@ -4,16 +4,27 @@ import com.university.booking_university_project.jpa.entity.Apartment;
 import com.university.booking_university_project.modules.apartment.repository.ApartmentRepository;
 import com.university.booking_university_project.modules.baseService.BaseServiceTest;
 import com.university.booking_university_project.testUtils.TestUtils;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class ApartmentServiceImplTest
     implements BaseServiceTest<Apartment, Integer, ApartmentServiceImpl> {
 
-  private final ApartmentRepository apartmentRepository;
+  @InjectMocks
+  private ApartmentServiceImpl apartmentService;
 
-  private final TestUtils testUtils;
+  @Mock
+  private ApartmentRepository apartmentRepository;
+
+  private TestUtils testUtils;
 
   public static final int TEST_NUMBER_OF_ROOMS = 3;
   public static final double TEST_SQUARE = 100.0;
@@ -23,26 +34,23 @@ public class ApartmentServiceImplTest
   public static final int TEST_NUMBER_OF_DOUBLE_BEDS = 2;
   public static final int TEST_NUMBER_OF_SINGLE_BEDS = 1;
 
-  @Autowired
-  public ApartmentServiceImplTest(ApartmentRepository apartmentRepository, TestUtils testUtils) {
-    this.apartmentRepository = apartmentRepository;
-    this.testUtils = testUtils;
-  }
+
 
   @Override
   public ApartmentServiceImpl createService() {
-    return new ApartmentServiceImpl(apartmentRepository);
+    Apartment entity = createEntity();
+    Apartment entity1 = createEntity();
+
+    when(entity.getId()).thenReturn(0);
+    when(entity1.getId()).thenReturn(1);
+    when(apartmentRepository.saveAll(anyCollection())).thenReturn(List.of(entity, entity1));
+    return apartmentService;
   }
 
   @Override
   public Apartment createEntity() {
-    return testUtils.createApartmentEntity(
-        TEST_NUMBER_OF_ROOMS,
-        TEST_SQUARE,
-        TEST_RENT_PRICE,
-        TEST_FLOOR,
-        TEST_DESCRIPTION,
-        TEST_NUMBER_OF_DOUBLE_BEDS,
-        TEST_NUMBER_OF_SINGLE_BEDS);
+    Apartment apartment = mock(Apartment.class);
+    when(apartment.getId()).thenReturn(null);
+    return apartment;
   }
 }
