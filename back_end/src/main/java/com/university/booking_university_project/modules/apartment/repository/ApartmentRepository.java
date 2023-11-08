fe535349eff9a074c304a6304f99d7c50b45b946
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-08 18:17:46 +0100

    add annotations

--- back_end/src/main/java/com/university/booking_university_project/modules/apartment/repository/ApartmentRepository.java
+++ back_end/src/main/java/com/university/booking_university_project/modules/apartment/repository/ApartmentRepository.java
@@ -2,5 +2,7 @@ package com.university.booking_university_project.modules.apartment.repository;
 
 import com.university.booking_university_project.jpa.entity.Apartment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ApartmentRepository extends JpaRepository<Apartment, Integer> {}
