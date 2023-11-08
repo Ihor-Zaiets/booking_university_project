162a45e16a21380fe402ae2b51f67b56108060a1
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-08 16:37:02 +0100

    authService update

--- back_end/src/main/java/com/university/booking_university_project/modules/auth/AuthService.java
+++ back_end/src/main/java/com/university/booking_university_project/modules/auth/AuthService.java
@@ -1,12 +1,7 @@
 package com.university.booking_university_project.modules.auth;
 
 import com.university.booking_university_project.modules.user.dto.UserRegistrationRequest;
-import org.springframework.stereotype.Service;
 
-@Service
-public class AuthService {
-
-  public void signUp(UserRegistrationRequest registrationRequest) {
-
-  }
+public interface AuthService {
+  void signUp(UserRegistrationRequest userRegistrationRequest);
 }
---
+++ back_end/src/main/java/com/university/booking_university_project/modules/auth/AuthServiceImpl.java
@@ -0,0 +1,13 @@
+package com.university.booking_university_project.modules.auth;
+
+import com.university.booking_university_project.modules.user.dto.UserRegistrationRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+  @Override
+  public void signUp(UserRegistrationRequest registrationRequest) {
+
+  }
+}
