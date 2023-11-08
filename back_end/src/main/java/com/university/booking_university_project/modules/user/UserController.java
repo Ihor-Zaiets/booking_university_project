9020cd7c8995d9d32db5f3232dd342ca7a241a1f
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-08 16:48:12 +0100

    user controller

--- back_end/src/main/java/com/university/booking_university_project/modules/user/UserController.java
+++ back_end/src/main/java/com/university/booking_university_project/modules/user/UserController.java
@@ -1,6 +1,43 @@
 package com.university.booking_university_project.modules.user;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.university.booking_university_project.modules.user.dto.UserCreationDTO;
+import com.university.booking_university_project.modules.user.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-public class UserController {}
+@RequestMapping("/api/User")
+public class UserController {
+
+  private final UserService userService;
+
+  @Autowired
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping("/createAll")
+  public ResponseEntity<List<UserDTO>> createUsers(@RequestBody List<UserCreationDTO> userCreationDTOList) {
+    return ResponseEntity.ok(userService.createUsers(userCreationDTOList));
+  }
+
+  @GetMapping("/searchAll")
+  public ResponseEntity<List<UserDTO>> findAllUsers() {
+    return ResponseEntity.ok(userService.findAllDTO());
+  }
+
+  @PostMapping("/editAll")
+  public ResponseEntity<List<UserDTO>> editUsers(@RequestBody UserCreationDTO userCreationDTO) {
+    return ResponseEntity.ok(userService.editUsers(userCreationDTO));
+  }
+
+  @DeleteMapping("/deleteAll")
+  public ResponseEntity<Void> deleteUsers(@RequestBody List<Integer> ids) {
+    userService.deleteAllByIds(ids);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
