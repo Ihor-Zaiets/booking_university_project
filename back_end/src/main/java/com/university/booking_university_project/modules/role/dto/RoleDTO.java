7ecf3e0a062ccd1ac43a382b3059236cf82bf670
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-09 21:19:20 +0100

    stackOverflowException fix

--- back_end/src/main/java/com/university/booking_university_project/modules/role/dto/RoleDTO.java
+++ back_end/src/main/java/com/university/booking_university_project/modules/role/dto/RoleDTO.java
@@ -1,3 +1,16 @@
 package com.university.booking_university_project.modules.role.dto;
 
-public class RoleDTO {}
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+
+@Setter
+@Getter
+public class RoleDTO {
+  
+  private Integer id;
+
+  @NonNull private String name;
+
+  private String description;
+}
--- back_end/src/main/java/com/university/booking_university_project/modules/user/dto/UserDTO.java
+++ back_end/src/main/java/com/university/booking_university_project/modules/user/dto/UserDTO.java
@@ -1,3 +1,31 @@
 package com.university.booking_university_project.modules.user.dto;
 
-public class UserDTO {}
+import com.university.booking_university_project.modules.role.dto.RoleDTO;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+
+@Getter
+@Setter
+public class UserDTO {
+
+  private Integer id;
+
+  private String login;
+
+  private String password;
+
+  private List<RoleDTO> roles;
+
+  @NonNull
+  private String firstname;
+
+  private String surname;
+
+  private String email;
+
+  @NonNull private String phone;
+
+  private String address;
+}
