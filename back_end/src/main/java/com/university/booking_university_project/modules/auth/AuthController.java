d045d6a50330912bb1abbf7e4e3412d43721eb22
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-10 08:00:41 +0100

    mapping update

--- back_end/src/main/java/com/university/booking_university_project/modules/auth/AuthController.java
+++ back_end/src/main/java/com/university/booking_university_project/modules/auth/AuthController.java
@@ -3,10 +3,7 @@ package com.university.booking_university_project.modules.auth;
 import com.university.booking_university_project.modules.user.dto.UserRegistrationRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -18,7 +15,7 @@ public class AuthController {
     this.authService = authService;
   }
 
-  @GetMapping("/signup")
+  @PostMapping("/signup")
   public ResponseEntity<Void> getResponseEntity(@RequestBody UserRegistrationRequest registrationRequest) {
     authService.signUp(registrationRequest);
     return new ResponseEntity<>(HttpStatus.OK);
