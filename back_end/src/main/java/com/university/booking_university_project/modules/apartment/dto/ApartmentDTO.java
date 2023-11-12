cd58d39f14d1d73ac33e1819ce14fd7f5e0de6fa
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-12 21:17:11 +0100

    fill ApartmentDTO with fields

--- back_end/src/main/java/com/university/booking_university_project/modules/apartment/dto/ApartmentDTO.java
+++ back_end/src/main/java/com/university/booking_university_project/modules/apartment/dto/ApartmentDTO.java
@@ -1,3 +1,26 @@
 package com.university.booking_university_project.modules.apartment.dto;
 
-public class ApartmentDTO {}
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+
+@Setter
+@Getter
+public class ApartmentDTO {
+  
+  private Integer id;
+
+  @NonNull private Integer numberOfRooms;
+
+  private Double square;
+
+  @NonNull private Double rentPrice;
+
+  private Integer floor;
+
+  private String description;
+
+  @NonNull private Integer numberOfDoubleBeds;
+
+  @NonNull private Integer numberOfSingleBeds;
+}
