1b29061adecf0bebf4ca015b20b83a7b56cec42f
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-20 18:35:42 +0100

    finish Angular learning video

--- front_end/src/app/components/todos/todos.component.ts
+++ front_end/src/app/components/todos/todos.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common';
 import { Todo } from '../../models/Todo'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent implements OnInit{
   todos:Todo[];
+
+  inputTodo:string = "";
 
   ngOnInit(): void {
     this.todos = [
@@ -23,5 +26,26 @@ export class TodosComponent implements OnInit{
         completed:true
       }
     ]
+  }
+
+  toggleDone (id:number) {
+    this.todos.map((todo, index) => {
+      if (id == index) todo.completed = !todo.completed;
+
+      return todo;
+    })
+  }
+
+  deleteTodo(id:number) {
+    this.todos = this.todos.filter((todo, index) => index !== id);
+  }
+
+  addTodo () {
+    this.todos.push({
+       content:this.inputTodo,
+       completed: false
+    })
+
+    this.inputTodo = "";
   }
 }
