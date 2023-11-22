a3657e3e541084af7f48889508f195f7757c0d67
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-22 19:35:45 +0100

    resolve cors policy problem

--- back_end/src/main/java/com/university/booking_university_project/configuration/SecurityConfiguration.java
+++ back_end/src/main/java/com/university/booking_university_project/configuration/SecurityConfiguration.java
@@ -2,10 +2,18 @@ package com.university.booking_university_project.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfiguration {
@@ -13,7 +21,10 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-    httpSecurity.csrf().disable().authorizeHttpRequests((authorize) ->
+    httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+            .authorizeHttpRequests((authorize) ->
             authorize
 //                    .requestMatchers("/api/auth/testString").hasAnyAuthority("ADMIN") //example
                     .anyRequest().permitAll())
@@ -24,6 +35,16 @@ public class SecurityConfiguration {
                             .permitAll());
 
     return httpSecurity.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:4200", "*"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "HEAD", "DELETE", "PATCH", "OPTIONS", "CONNECT", "*"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
