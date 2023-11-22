61426bbe903bba685c98de616d11f98edff60d7b
Ihor Zaiets <dekstroer8202@gmail.com>
2023-11-22 20:03:58 +0100

    sending POST methods from frontend (cors policy does not work)

--- front_end/src/app/user/user.component.ts
+++ front_end/src/app/user/user.component.ts
@@ -6,8 +6,28 @@ import { UserService } from './user.service'
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit{
+export class UserComponent implements OnInit {
   data: any;
+  postData: any = [
+    {
+      "id": 1,
+      "login": "admin2",
+      "password": "$2a$12$AhQoDd2zsJOxI5Ub4gZnFeuDTQHsNIORIteHYn0yeYzFSugxsdsHu",
+      "roles": [
+        {
+          "id": 1,
+          "name": "ADMIN",
+          "description": null
+        }
+      ],
+      "firstname": "admin2",
+      "email": "admin2@example.com",
+      "phone": "+1234567890",
+      "address": ""
+    }
+  ];
+
+  postDataResponse = "Response from backend on POST"
 
   constructor(private userService: UserService) {}
 
@@ -21,5 +41,20 @@ export class UserComponent implements OnInit{
       console.error('An error occurred:', error);
     }
       );
+  }
+
+  submitPostRequest() {
+    this.userService.postData(this.postData).subscribe(
+      (response) => {
+        console.log('POST request successful:', response);
+        this.postDataResponse = 'POST request successful';
+        // Handle the response from the backend as needed
+      },
+      (error) => {
+        console.error('Error during POST request:', error);
+        this.postDataResponse = 'Error during POST request';
+        // Handle errors, if any
+      }
+    );
   }
 }
--- front_end/src/app/user/user.service.ts
+++ front_end/src/app/user/user.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import { BasicUrlLink } from '../BasicUrlLink'
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +11,12 @@ export class UserService {
 
   getData() {
     return this.http.get(BasicUrlLink.basicLink + '/api/User/searchAll');
+  }
+
+  postData(requestBody: any): Observable<any> {
+    const url = BasicUrlLink.basicLink + `/api/User/createAll`; // replace with your specific endpoint
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.http.post<any>(url, requestBody, { headers });
   }
 }
