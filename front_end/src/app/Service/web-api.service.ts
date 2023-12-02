7ba1193c020fc691e5077017b82962b62fdc8287
Ihor Zaiets <dekstroer8202@gmail.com>
2023-12-02 15:40:14 +0100

    add toastr to web-api.service.ts and refactor code

--- front_end/src/app/Service/web-api.service.ts
+++ front_end/src/app/Service/web-api.service.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { catchError, map, Observable, throwError } from 'rxjs'
+import { ToastrService } from 'ngx-toastr'
+
+const httpGetOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Cache-Control' : 'no-cache',
+    'Pragma' : 'no-cache'
+  }),
+  observe: "response" as 'body'
+};
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  }),
+  observe: "response" as 'body'
+};
+
+const httpDeleteOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'}),
+  observe: "response" as 'body',
+  body: null,
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebApiService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private toastr: ToastrService) { }
 
   get(url: string): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'Cache-Control' : 'no-cache',
-        'Pragma' : 'no-cache'
-      }),
-      observe: "response" as 'body'
-    };
-    return this.httpClient.get(url, httpOptions)
+    return this.httpClient.get(url, httpGetOptions)
       .pipe(
         map((response: any) => this.ReturnResponseData(response)),
         catchError(this.handleError)
@@ -26,12 +41,6 @@ export class WebApiService {
   }
 
   post(url: string, requestBody: any): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      }),
-      observe: "response" as 'body'
-    };
     return this.httpClient.post(url, requestBody, httpOptions)
       .pipe(
         map((response: any) => this.ReturnResponseData(response)),
@@ -40,13 +49,6 @@ export class WebApiService {
   }
 
   patch(url: string, requestBody: any): Observable<any> {
-    const httpOptions = {
-      headers:
-        new HttpHeaders({
-          'Content-Type': 'application/json'
-        }),
-      observe: "response" as 'body'
-    };
     return this.httpClient.patch(url, requestBody, httpOptions)
       .pipe(
         map((response: any) => this.ReturnResponseData(response)),
@@ -55,13 +57,8 @@ export class WebApiService {
   }
 
   delete(url: string, requestBody: any): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({'Content-Type': 'application/json'}),
-      observe: "response" as 'body',
-      body: requestBody,
-    }
-
-    return this.httpClient.delete(url, httpOptions)
+    httpDeleteOptions.body = requestBody;
+    return this.httpClient.delete(url, httpDeleteOptions)
       .pipe(
         map((response: any) => this.ReturnResponseData(response)),
         catchError(this.handleError)
