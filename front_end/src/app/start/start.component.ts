45a9abe5430aeaf2dfd578aa111edaf086103e19
Ihor Zaiets <dekstroer8202@gmail.com>
2023-12-04 20:53:43 +0100

    add apartments to start site

--- front_end/src/app/start/start.component.ts
+++ front_end/src/app/start/start.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Apartment } from '../model/Apartment'
+import { ApartmentService } from '../apartment/apartment.service'
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss']
 })
-export class StartComponent {
+export class StartComponent implements OnInit {
+  apartments!: Apartment[];
+  checkedApartments: Set<number> = new Set<number>();
 
+  constructor(private apartmentService: ApartmentService) {}
+
+  ngOnInit(): void {
+    this.getAllApartments();
+  }
+
+  getAllApartments() {
+    this.apartmentService.getAllApartments().subscribe((response) => this.apartments = response.body);
+  }
+
+  isChecked(apartmentId: number) {
+    return this.checkedApartments.has(apartmentId);
+  }
+
+  onCheckboxChange(apartmentId: number) {
+    if (this.checkedApartments.has(apartmentId)) {
+      this.checkedApartments.delete(apartmentId);
+    } else {
+      this.checkedApartments.add(apartmentId);
+    }
+  }
 }
