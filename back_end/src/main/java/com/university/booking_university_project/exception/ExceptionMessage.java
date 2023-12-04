f1c147af170848fadbc0335d4952bade16a183bf
Ihor Zaiets <dekstroer8202@gmail.com>
2023-12-04 20:41:18 +0100

    translate back-end to polish

--- back_end/src/main/java/com/university/booking_university_project/exception/ExceptionMessage.java
+++ back_end/src/main/java/com/university/booking_university_project/exception/ExceptionMessage.java
@@ -1,18 +1,18 @@
 package com.university.booking_university_project.exception;
 
 public class ExceptionMessage {
-  public static final String TRIM_VALIDATION_MESSAGE = "String contains white spaces at beginning or end";
-  public static final String USER_FIRSTNAME_DOES_NOT_MATCH_PATTERN = "User firstname bad pattern";
-  public static final String FIELD_NULL_OR_EMPTY_VALIDATION_MESSAGE = "Field is null or empty";
-  public static final String USER_SURNAME_VALIDATION_MESSAGE = "User surname is null or empty";
-  public static final String EMAIL_VALIDATION_MESSAGE = "Email does not match pattern";
-  public static final String PHONE_NUMBER_VALIDATION_MESSAGE = "Phone number does not match pattern";
-  public static final String EMAIL_ALREADY_EXISTS_VALIDATION_MESSAGE = "Email already exists";
-  public static final String ROLE_ALREADY_EXISTS_VALIDATION_MESSAGE = "Role already exists";
-  public static final String PHONE_ALREADY_EXISTS_VALIDATION_MESSAGE = "Phone number already exists";
-  public static final String USER_NOT_FOUND_EXCEPTION = "User not found";
+  public static final String TRIM_VALIDATION_MESSAGE = "Napis zawiera białe znaki na początku lub końcu";
+  public static final String USER_FIRSTNAME_DOES_NOT_MATCH_PATTERN = "Nieprawidłowy wzorzec imienia użytkownika";
+  public static final String FIELD_NULL_OR_EMPTY_VALIDATION_MESSAGE = "Pole jest puste lub null";
+  public static final String USER_SURNAME_VALIDATION_MESSAGE = "Nazwisko użytkownika jest puste lub null";
+  public static final String EMAIL_VALIDATION_MESSAGE = "Adres e-mail nie spełnia wzorca";
+  public static final String PHONE_NUMBER_VALIDATION_MESSAGE = "Numer telefonu nie spełnia wzorca";
+  public static final String EMAIL_ALREADY_EXISTS_VALIDATION_MESSAGE = "Adres e-mail już istnieje";
+  public static final String ROLE_ALREADY_EXISTS_VALIDATION_MESSAGE = "Rola już istnieje";
+  public static final String PHONE_ALREADY_EXISTS_VALIDATION_MESSAGE = "Numer telefonu już istnieje";
+  public static final String USER_NOT_FOUND_EXCEPTION = "Użytkownik nie został znaleziony";
 
-  public static final String WRONG_NUMERIC_VALUE = "Wrong numeric value";
-  public static final String END_DATE_BEFORE_START_DATE = "End date is before start date";
-  public static final String RESERVATION_OVERLAY = "Reservation for that hotel in that time already exists";
+  public static final String WRONG_NUMERIC_VALUE = "Nieprawidłowa wartość numeryczna";
+  public static final String END_DATE_BEFORE_START_DATE = "Data końcowa jest przed datą początkową";
+  public static final String RESERVATION_OVERLAY = "Rezerwacja dla tego hotelu w tym czasie już istnieje";
 }
