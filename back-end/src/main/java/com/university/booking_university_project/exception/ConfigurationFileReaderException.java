aa5c418886f6f42490a5fe02bc6852ea6b277522
Ihor Zaiets <dekstroer8202@gmail.com>
2024-09-26 14:47:32 +0200

    update exception message

--- back-end/src/main/java/com/university/booking_university_project/exception/ConfigurationFileReaderException.java
+++ back-end/src/main/java/com/university/booking_university_project/exception/ConfigurationFileReaderException.java
@@ -3,7 +3,7 @@ package com.university.booking_university_project.exception;
 public class ConfigurationFileReaderException extends RuntimeException {
 
     public ConfigurationFileReaderException(String fileName) {
-        super("No such file found: " + fileName);
+        super("No such file found: " + fileName + ". Check if " + fileName + " exists in project resources.");
     }
 
     public ConfigurationFileReaderException(String propertyName, String fileName) {
